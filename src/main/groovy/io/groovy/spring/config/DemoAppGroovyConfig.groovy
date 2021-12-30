257d1f11d39fc3f162c78bd9a4a4d7df54ac2ed5
Edgar Vardanyan <e.vardan4an@gmail.com>
2021-12-30 22:01:21 +0400

    update progress adn added messages.properties

--- src/main/groovy/io/groovy/spring/config/DemoAppGroovyConfig.groovy
+++ src/main/groovy/io/groovy/spring/config/DemoAppGroovyConfig.groovy
@@ -4,7 +4,9 @@ package io.groovy.spring.config
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration
-
+import org.springframework.context.annotation.PropertySource
+import org.springframework.context.support.ReloadableResourceBundleMessageSource
+import org.springframework.context.support.ResourceBundleMessageSource
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 ;
@@ -15,6 +17,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="io.groovy.spring")
+//@PropertySource("classpath:messages.properties")
 class DemoAppGroovyConfig {
 
     // define a bean for ViewResolver
@@ -25,6 +28,26 @@ class DemoAppGroovyConfig {
         viewResolver.setPrefix("/WEB-INF/view/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+
+//    @Bean(name = "messageSource")
+//    ReloadableResourceBundleMessageSource getMessageSource() {
+//        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+//        messageSource.setBasename("resources/messages");
+//        messageSource.setDefaultEncoding("UTF-8");
+//        messageSource.setUseCodeAsDefaultMessage(true);
+//        return messageSource;
+//    }
+
+    @Bean
+    ResourceBundleMessageSource messageSource() {
+
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true)
+        return messageSource;
     }
 
 }
--- src/main/groovy/io/groovy/spring/model/Customer.groovy
+++ src/main/groovy/io/groovy/spring/model/Customer.groovy
@@ -1,6 +1,9 @@
 package io.groovy.spring.model
 
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
 
@@ -15,5 +18,14 @@ class Customer {
     @NotNull(message="is required")
     @Size(min=1, message="is required")
     String lastName
+
+    @NotNull(message="is required")
+    @Min(value=0L, message = "must be greater then or equal to zero")
+    @Max(value=10L, message = "must be less then or equal to 10")
+    Integer freePasses
+
+    @Pattern(regexp = "^[a-zA-z0-0]{5}", message="only 5 chars/digits")
+    String postalCode
+
 
 }
